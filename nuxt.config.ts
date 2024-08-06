ebc5faf16e361af31443564f16d4f5f43e188752
Lokusok <lokusokdrobk@gmail.com>
2024-08-06 10:41:40 +0300

    add prisma

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,16 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
   modules: [
+    '@nuxtjs/supabase',
     '@nuxtjs/tailwindcss',
     '@nuxt/icon',
     '@pinia/nuxt',
     '@prisma/nuxt',
     '@vite-pwa/nuxt',
   ],
+  runtimeConfig: {
+    public: {
+      bucketUrl: process.env.BUCKET_URL,
+    },
+  },
 });
